56f18c041793524181b28699321ac240e8b6f0e3
Omar Khalefa <Khalefa@DESKTOP-S9RHSS4>
2024-04-26 10:20:19 +0300

    Update From .net5.0 To .net8.0

--- Client.PL/Program.cs
+++ Client.PL/Program.cs
@@ -1,26 +1,98 @@
-using Microsoft.AspNetCore.Hosting;
+
+using Client.DAL.Data;
+using Client.DAL.Models;
+using Client.PL.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Client.PL
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			var WebApplicationbuilder = WebApplication.CreateBuilder(args);
+
+			#region Configure Services
+
+			WebApplicationbuilder.Services.AddControllersWithViews(); //  Register built-in Services Required By MVC
+
+			///		WebApplicationbuilder.Services.AddScoped<ApplicationDbContext>();
+			///		WebApplicationbuilder.Services.AddScoped<DbContextOptions<ApplicationDbContext>>();
+
+			///		WebApplicationbuilder.Services.AddDbContext<ApplicationDbContext>
+			///    (
+			///    contextLifetime: ServiceLifetime.Scoped,
+			///    optionsLifetime: ServiceLifetime.Scoped
+			///    );
+
+
+			// 		WebApplicationbuilder.Services.AddAutoMapper(M => M.AddProfile(new MappingProfAiles()));
+
+			WebApplicationbuilder.Services.AddDbContext<ApplicationDbContext>(
+			 options => options.UseSqlServer(WebApplicationbuilder.Configuration.GetConnectionString("DefaultConnection"))
+											   );
+
+			//ApplicationServicesExtensions.AddApplicationServices(		WebApplicationbuilder.Services);
+			WebApplicationbuilder.Services.AddApplicationServices();
+
+			WebApplicationbuilder.Services.AddIdentity<ApplicationUser, IdentityRole>(Options =>
+			 {
+				 Options.Password.RequireDigit = true;
+				 Options.Lockout.AllowedForNewUsers = true;
+				 Options.Lockout.MaxFailedAccessAttempts = 5;
+				 Options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(5);
+			 }).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+			WebApplicationbuilder.Services.ConfigureApplicationCookie(options =>
+						 {
+							 options.LoginPath = "/Account/SignIn";
+							 options.ExpireTimeSpan = TimeSpan.FromDays(1);
+							 options.AccessDeniedPath = "/Home/Error";
+						 });
+			WebApplicationbuilder.Services.AddAuthentication();// Will Be Call With AddIdentity We Use It IF We Need The Other Overlodes
+
+			#endregion
+
+			#region Configure Kestrel MiddelWears
+			var app = WebApplicationbuilder.Build();
+
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+			#endregion
+
+			app.Run();
+		}
+
+
+	}
 }
--- Client.PL/Startup.cs
+++ Client.PL/Startup.cs
@@ -24,72 +24,13 @@ namespace Client.PL
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews(); //  Register built-in Services Required By MVC
-
-			///services.AddScoped<ApplicationDbContext>();
-			///services.AddScoped<DbContextOptions<ApplicationDbContext>>();
-
-			///services.AddDbContext<ApplicationDbContext>
-			///    (
-			///    contextLifetime: ServiceLifetime.Scoped,
-			///    optionsLifetime: ServiceLifetime.Scoped
-			///    );
-
-
-			// services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
-
-			services.AddDbContext<ApplicationDbContext>(
-				options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-													   );
-
-			//ApplicationServicesExtensions.AddApplicationServices(services);
-			services.AddApplicationServices();
-
-			services.AddIdentity<ApplicationUser, IdentityRole>(Options =>
-			{
-				Options.Password.RequireDigit = true;
-				Options.Lockout.AllowedForNewUsers = true;
-				Options.Lockout.MaxFailedAccessAttempts = 5;
-				Options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(5);
-			}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-
-			services.ConfigureApplicationCookie(options =>
-			{
-				options.LoginPath = "/Account/SignIn";
-				options.ExpireTimeSpan = TimeSpan.FromDays(1);
-				options.AccessDeniedPath = "/Home/Error";
-			});
-			 services.AddAuthentication();// Will Be Call With AddIdentity We Use It IF We Need The Other Overlodes
+		
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-			app.UseHttpsRedirection();
-
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
-			app.UseAuthentication();
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
-			});
+			
 		}
 	}
 }
