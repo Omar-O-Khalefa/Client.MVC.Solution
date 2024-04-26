440301f877ec83651758e0e4a35455408bd3a298
Omar Khalefa <Khalefa@DESKTOP-S9RHSS4>
2024-04-26 07:52:41 +0300

    SendEmail

---
+++ Client.PL/services/EmailSender/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Client.PL.services.EmailSender
+{
+	public class EmailSender : IEmailSender
+	{
+		private readonly IConfiguration _configuration;
+
+		public EmailSender(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public async Task SendAsync(string form, string recipents, string subject, string body)
+		{
+			var senderEmail = _configuration["EmailSettings:EmailSender"];
+			var senderPassword = _configuration["EmailSettings:EmailPassword"];
+
+			var emailMessage = new MailMessage();
+			emailMessage.From = new MailAddress(form);
+			emailMessage.To.Add (recipents);
+			emailMessage.Subject = subject;
+			emailMessage.Body = $"<html><body>{body}</body></html>";
+			emailMessage.IsBodyHtml = true;
+
+			var smtpClient = new SmtpClient(_configuration["EmailSettings:SmtpClientServer"], int.Parse(_configuration["EmailSettings:SmtpClientPort"]))
+			{
+				Credentials = new NetworkCredential(senderEmail,senderPassword),
+				EnableSsl = true
+			};
+
+		}
+	}
+}
---
+++ Client.PL/services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Client.PL.services.EmailSender
+{
+	public interface IEmailSender
+	{
+		Task SendAsync(string form,string recipents , string subject , string body);
+	}
+}
